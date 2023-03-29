be14bfef9777e7675b835841805a414aa2f80982
GE-Developer <on-line-misha@mail.ru>
2023-03-29 12:53:11 +0400

    Initial Commit

---
+++ WorkoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by M I C H A E L on 29.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
